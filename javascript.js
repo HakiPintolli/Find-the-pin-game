e551e27e09b04bbb7b864bc84c48e66d8950baa7
Haki Pintolli <155005609+HakiPintolli@users.noreply.github.com>
2024-07-18 00:47:18 +0200

    New look

--- javascript.js
+++ javascript.js
@@ -6,47 +6,42 @@ document.addEventListener("DOMContentLoaded", function () {
         const min = 1;
         const max = 9;
         const randomNum1 = Math.floor(Math.random() * (max - min + 1) + min);
-
-        const num1 = 0;
-        const num2 = 0;
-        const num3 = 0;
-        const num4 = 0;
-
+        const randomNum2 = Math.floor(Math.random() * (max - min + 1) + min);
+        const randomNum3 = Math.floor(Math.random() * (max - min + 1) + min);
+        const randomNum4 = Math.floor(Math.random() * (max - min + 1) + min);
+        const randomNum5 = Math.floor(Math.random() * (max - min + 1) + min);
+        const randomNum6 = Math.floor(Math.random() * (max - min + 1) + min);
+        const randomNum7 = Math.floor(Math.random() * (max - min + 1) + min);
+        
+        
         for (let i = 1; i <= 15; i++) {
             const listItem = document.getElementById(`${i}`);
             
-            const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
-            listItem.textContent = randomNum;
             
-            if (i === 1 || i === 4) {
+
+            if(i == 1 || i == 4 || i== 9){
                 listItem.textContent = randomNum1;
             }
 
-            if(randomNum === 1){
-                num1++;
+            else if(i== 2 || i == 12 || i == 14){
+                listItem.textContent = randomNum2;
+                }
+            
+            else if(i==3 || i== 7){
+                listItem.textContent = randomNum3;
             }
-            else if(randomNum === 2){
-                num2++;
+            else if(i==10 || i== 13){
+                listItem.textContent = randomNum4;
             }
-            else if(randomNum === 3){
-                num3++;
+            else if(i==5){
+                listItem.textContent = randomNum5;
             }
-            else if(randomNum === 4){
-                num4++;
+            else if(i==8 ||i== 15){
+                listItem.textContent = randomNum6;
             }
-            if(num1 === 4){
-                continue;
+            else{
+                listItem.textContent = randomNum7;
             }
-            else if(num2 === 4){
-                continue;
-            }
-            else if(num3 === 4){
-                continue;
-            }
-            else if(num4 === 4){
-                continue;
-            }
-
         }
     });
 });
